359b63e34bfb4a476b15efa2f8ea58d6a2fd65d7
Thomas Paul <nivintpaul@gmail.com>
2020-07-06 15:41:26 +0530

    now supports most of yolo gt formats

--- yolo_gt_conversion/convert_gt.py
+++ yolo_gt_conversion/convert_gt.py
@@ -1,5 +1,39 @@
-from pathlib import Path
+'''
+[1]
+darknet_format
+    one row per object
+    class x_center y_center width height
+    normalised
+    class number starts from 0
 
+class x_center y_center width height
+1 0.716797 0.395833 0.216406 0.147222
+0 0.687109 0.379167 0.255469 0.158333
+1 0.420312 0.395833 0.140625 0.166667
+
+../coco/images/train2017/000000109622.jpg  # image
+../coco/labels/train2017/000000109622.txt  # label
+
+train.txt contains training data
+test.txt contains testing data
+
+[2]
+YunYang format
+xxx/xxx.jpg 18.19,6.32,424.13,421.83,20 323.86,2.65,640.0,421.94,20 
+xxx/xxx.jpg 48,240,195,371,11 8,12,352,498,14
+# image_path x_min, y_min, x_max, y_max, class_id  x_min, y_min ,..., class_id 
+# make sure that x_max < width and y_max < height
+
+[3]
+csv file
+each bbox has following entry
+Image	Object Name	Object Index	bx	by	bw	bh	
+plan: create a pd with this info
+'''
+
+IS_FLATTEN = True
+
+from pathlib import Path
 def write_list(out_path, data):
     with open(out_path, mode='wt', encoding='utf-8') as fp:
         fp.write('\n'.join(data))
@@ -10,7 +44,7 @@ def read_labels(input_path):
     header = data[0]
     data = data[1:]
     assert len(data) == 84 * 60, "Number of files and labels don't match"
-    return data
+    return data, header
 
 def get_info(data):
     data = data.split(";")
@@ -20,45 +54,202 @@ def get_info(data):
         b_boxes = eval(data[-1])
     return id_seq, id_frame, b_boxes
 
-def covt_ln_darknet_format(data, input_dir, id_class=1, is_filter=False):
-    id_seq, id_frame, b_boxes = get_info(data)
+def convert(im_w, im_h, x_min, x_max, y_min, y_max):
+    """
+    Converts bbox into centroid coordinates relative to image dimensions
+    """
+    dw = 1./im_w
+    dh = 1./im_h
+    x = (x_min + x_max)/2.0
+    y = (y_min + y_max)/2.0
+    w = x_max - x_min
+    h = y_max - y_min
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return [x,y,w,h]
+
+def data2yolo(data):
+    if data[-1] is None:
+        return data
+    im_w = 840
+    im_h = 600
+    res = list(map(lambda x : convert(im_w, im_h, x_min=x[0], x_max=x[2], y_min=x[1], y_max=x[3]), data[-1]))
+    return [data[0], data[1], res]
+
+def write_yolo_data(data, img_dir):
+    global IS_FLATTEN
+    img_dir = Path(img_dir)
+    for seq, frame, bboxes in data:
+        if bboxes is None:
+            continue
+        
+        if IS_FLATTEN is False:
+            out_path = img_dir.joinpath("seq{}".format(seq))
+            out_path.mkdir(parents=True, exist_ok=True)
+            out_path = out_path.joinpath("img{}.txt".format(frame))
+        else:
+            out_path = img_dir.joinpath("seq{}_img{}.txt".format(seq, frame))
+
+        cvt_bbox = lambda x: "0 {}".format(" ".join(list(map(str, x)))).strip(" ")
+        bboxes = list(map(cvt_bbox, bboxes))
+        write_list(out_path, bboxes)
+
+def get_labels_format(data):
+    seq, frame, bboxes = data
+    ln = "{};{}".format(seq, frame)
+    if bboxes is not None:
+        bboxes = str(bboxes)
+        ln = "{};{}".format(ln, bboxes)
+    return ln
+
+def covt_ln_darknet_format(data, input_dir, id_class=0):
+    id_seq, id_frame, b_boxes = data
     bb_info = ""
     if b_boxes is not None:
         b_boxes = list(map(lambda x: ",".join(map(str, x)) + ",{}".format(id_class), b_boxes))
         bb_info = " ".join(b_boxes)
-    elif is_filter is True:
-        return ""
-    img_path = Path(input_dir).joinpath("seq{}".format(id_seq)).joinpath("img{}.jpg".format(id_frame))
+    if IS_FLATTEN is False:
+        img_path = Path(input_dir).joinpath("seq{}".format(id_seq)).joinpath("img{}.jpg".format(id_frame))
+    else:
+        img_path = Path(input_dir).joinpath("seq{}_img{}.jpg".format(id_seq, id_frame))
+
     converted_txt = "{} {}".format(img_path, bb_info)
     return converted_txt.strip()
 
-def convert_to_darknet_format(data, input_dir, id_class, is_filter, out_path):
-    """
-    Takes in the current label format @param: data and create a darknet
-    style GT format with relative path @param:input_dir, class id
-    @param: id_class. The output file will be written at @param:
-    out_path. If @param: is_filter is True, filters images without
-    detections.
-    """
-    converter = lambda x: covt_ln_darknet_format(x, input_dir, id_class, is_filter)
-    result = list(map(converter, data))
-    num_imgs = len(result)
-    if is_filter:
-        result = [x for x in result if x != '']
-    num_filtered = len(result)
-    write_list(out_path=out_path, data=result)
-    print("Created darknet format gt with {} samples. {} samples is filtered out.".format(num_imgs, num_imgs - num_filtered))
-    return result
+
+import xml.etree.cElementTree as ET
+
+def create_root(file_prefix, width, height):
+    file_base_path = Path(file_prefix)
+    root = ET.Element("annotation")
+    ET.SubElement(root, "folder").text = str(file_base_path.parent)
+    ET.SubElement(root, "filename").text = "{}.jpg".format(str(file_base_path.stem))
+    ET.SubElement(root, "path").text = "{}.jpg".format(str(file_base_path))
+    size = ET.SubElement(root, "size")
+    ET.SubElement(size, "width").text = str(width)
+    ET.SubElement(size, "height").text = str(height)
+    return root
+
+
+def create_object_annotation(root, voc_labels):
+    for voc_label in voc_labels:
+        obj = ET.SubElement(root, "object")
+        ET.SubElement(obj, "name").text=str(voc_label[0])
+        bbox = ET.SubElement(obj, "bndbox")
+        ET.SubElement(bbox, "xmin").text = str(voc_label[1])
+        ET.SubElement(bbox, "ymin").text = str(voc_label[2])
+        ET.SubElement(bbox, "xmax").text = str(voc_label[3])
+        ET.SubElement(bbox, "ymax").text = str(voc_label[4])
+    return root
+
+
+def create_file(file_prefix, width, height, voc_labels, output_dir):
+    root = create_root(file_prefix, width, height)
+    root = create_object_annotation(root, voc_labels)
+    tree = ET.ElementTree(root)
+    out_path = Path(output_dir)
+    out_path = out_path.joinpath(Path(file_prefix).parent)
+    out_path.mkdir(parents=True, exist_ok=True)
+    out_path = out_path.joinpath(file_prefix.split("/")[-1])
+    tree.write("{}.xml".format(out_path))
+
+
+def create_xml_file(file_path, output_dir):
+    file_prefix = file_path.split(".jpg")[0]
+    w, h = 840, 600
+    try:
+        with open("{}.txt".format(file_prefix), 'r') as file:
+            lines = file.readlines()
+    except Exception as err:
+        lines = []
+
+    voc_labels = []
+    for line in lines:
+        voc = []
+        line = line.strip()
+        data = line.split()
+        voc.append('fish')
+        bbox_width = float(data[3]) * w
+        bbox_height = float(data[4]) * h
+        center_x = float(data[1]) * w
+        center_y = float(data[2]) * h
+        voc.append(round(center_x - (bbox_width / 2)))
+        voc.append(round(center_y - (bbox_height / 2)))
+        voc.append(round(center_x + (bbox_width / 2)))
+        voc.append(round(center_y + (bbox_height / 2)))
+        voc_labels.append(voc)
+    create_file(file_prefix, w, h, voc_labels, output_dir)
+
+
+def generate_gt(input_path="./train/labels.csv", test_percent = 0.2, img_dir = "./out", output_dir = "./gt_output"):    
+    img_dir = Path(img_dir)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    full_data, header = read_labels(input_path=input_path)
+    full_data = list(map(lambda x: get_info(x), full_data))
+    filtered_data = [x for x in full_data if x[-1] is not None]
+
+    for data, data_name in zip([full_data, filtered_data], ["full", "filtered"]):
+        
+        # Each dataset split into train and test
+        num_samples = len(data)
+        num_train = int((1 - test_percent) * num_samples)
+        train_samples = data[:num_train]
+        test_samples = data[num_train:]
+
+        # Generating evaluation labels.csv
+        for samples, sample_category in zip([train_samples, test_samples], ["train", "test"]):
+            out_path = "{}/evaluation_labels_{}_{}_split_{}.csv".format(output_dir, data_name, sample_category, test_percent)
+            eval_csv_data = list(map(get_labels_format, samples))
+            eval_csv_data.insert(0, header)
+            write_list(out_path, eval_csv_data)
+
+        # Generating evaluation labels.csv
+        for samples, sample_category in zip([train_samples, test_samples], ["train", "test"]):
+            out_path = "{}/yun_yang_format_{}_{}_split_{}.txt".format(output_dir, data_name, sample_category, test_percent)
+            yun_yang_data = list(map(lambda  x: covt_ln_darknet_format(x, input_dir=img_dir, id_class=0), samples))
+            write_list(out_path, yun_yang_data)
+        
+        # YOLO format
+        for samples, sample_category in zip([train_samples, test_samples], ["train", "test"]):
+            print("Generating YOLO gt for {} {} dataset.".format(sample_category, data_name))
+            yolo_data = list(map(lambda x: data2yolo(x), samples))
+            # write the yolo data along with images
+            write_yolo_data(yolo_data, img_dir=img_dir)
+            if IS_FLATTEN is not True:
+                get_img_path = lambda x: str(img_dir.joinpath("seq{}".format(x[0])).joinpath("img{}.jpg".format(x[1])))
+            else:
+                get_img_path = lambda x: str(img_dir.joinpath("seq{}_img{}.jpg".format(x[0], x[1])))
+            sample_img_paths = list(map(get_img_path, samples))
+            out_path = "{}/yolo_{}_{}_split_{}.txt".format(output_dir, data_name, sample_category, test_percent)
+            write_list(out_path, sample_img_paths)
+        
+            # VOC Pascal format
+            for sample_img_path in sample_img_paths:
+                create_xml_file(file_path=sample_img_path, output_dir="{}/xml_labels".format(output_dir))
+
+from glob import glob
+from shutil import copyfile
+
+def flattern_dir_struture(input_dir, output_dir):
+    input_paths = glob(input_dir +  "/*/*.jpg")
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    copyfile(input_dir.joinpath("labels.csv"), output_dir.joinpath("labels.csv"))
+    for img_path in input_paths:
+        img_path = Path(img_path)
+        img_name = img_path.name
+        seq = img_path.parent.stem
+        dst_path = output_dir.joinpath("{}_{}".format(seq, img_name))
+        copyfile(img_path, dst_path)
+
 
 if __name__ == "__main__":
-    data = read_labels(input_path="./labels.csv")
-    # Use below function to convert to darknet GT format.
-    # The input_dir should be the path to dataset train.
-    # Created darknet format gt with 5040 samples. 2345 samples is filtered out.
-    # Created darknet format gt with 5040 samples. 0 samples is filtered out.
-    convert_to_darknet_format(data,
-                              input_dir="dataset/train/",
-                              id_class=1,
-                              is_filter=False,
-                              out_path="yolo_darknet_gt.txt")
-    
+    flattern_dir_struture(input_dir="/home/master/dataset/train", output_dir="./yolo_dataset")
+    generate_gt(input_path="./yolo_dataset/labels.csv",
+                test_percent = 0.2,
+                img_dir = "./yolo_dataset",
+                output_dir = "./yolo_gt")
